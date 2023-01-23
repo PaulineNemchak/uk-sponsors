fc8118ab25c4b92741bef757c8ba9f3a3c8b50e4
pauline nemchak <doublewebstandards@gmail.com>
2023-01-23 03:18:21 +0100

    inital app

---
+++ src/components/App/App.tsx
@@ -0,0 +1,10 @@
+import { Table } from '../Table/Table';
+
+export function App() {
+    return (
+        <>
+            <h1>List</h1>
+            <Table />
+        </>
+  );
+}
---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,44 @@
+export const Table = () => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Organisation Name</th>
+                    <th>Town/City</th>
+                    <th>County</th>
+                    <th>Route</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        415 PALAETTE LTD
+                    </td>
+                    <td>
+                        London
+                    </td>
+                    <td>
+                        England
+                    </td>
+                    <td>
+                        Skilled Worker
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Above Bar Church
+                    </td>
+                    <td>
+                        Southampton
+                    </td>
+                    <td>
+                        Hampshire
+                    </td>
+                    <td>
+                        Religious Worker
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
