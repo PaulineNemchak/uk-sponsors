0d3161063af042e39411fd642613191780ce3131
pauline nemchak <doublewebstandards@gmail.com>
2023-02-05 02:36:23 +0100

    added basic types and styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,11 @@
 import { Table } from '../Table/Table';
+import { Container } from "../Container/Container";
 
 export function App() {
     return (
-        <>
+        <Container>
             <h1>List</h1>
             <Table />
-        </>
+        </Container>
   );
 }
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,44 +1,106 @@
+import styles from './Table.module.css';
+
+const routes: string[] = [
+    'Skilled worker',
+    'Creative Worker',
+    'Global Business Mobility',
+    'Tier 2 Ministers of Religion'
+];
+
+type Employer = {
+    id: number;
+    name: string;
+    city: string;
+    county: string;
+    rating: string;
+    route: string;
+}
+
+type Data = {
+    content: Employer[];
+    pageNum: number;
+    pageSize: number;
+    totalElementsCount: number;
+    totalPagesCount: number;
+    lastPage: boolean;
+}
+
+const data: Data = {
+    "content" : [ {
+        "id" : 68845,
+        "name" : "001 Times Ltd",
+        "city" : "London",
+        "county" : "Hampshire",
+        "rating" : "Temporary Worker (A rating)",
+        "route" : "Creative Worker"
+    }, {
+        "id" : 34445,
+        "name" : "001 Times Ltd",
+        "city" : "London",
+        "county" : "",
+        "rating" : "Worker (A rating)",
+        "route" : "Skilled Worker"
+    }, {
+        "id" : 40502,
+        "name" : "003 Ltd",
+        "city" : "Hounslow",
+        "county" : "",
+        "rating" : "Worker (A rating)",
+        "route" : "Skilled Worker"
+    } ],
+    "pageNum" : 2,
+    "pageSize" : 3,
+    "totalElementsCount" : 69137,
+    "totalPagesCount" : 23046,
+    "lastPage" : false
+}
+
 export const Table = () => {
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Organisation Name</th>
-                    <th>Town/City</th>
-                    <th>County</th>
-                    <th>Route</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        415 PALAETTE LTD
-                    </td>
-                    <td>
-                        London
-                    </td>
-                    <td>
-                        England
-                    </td>
-                    <td>
-                        Skilled Worker
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Above Bar Church
-                    </td>
-                    <td>
-                        Southampton
-                    </td>
-                    <td>
-                        Hampshire
-                    </td>
-                    <td>
-                        Religious Worker
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    )
+    return <table className={styles.table}>
+        <thead>
+            <tr>
+                <th>Organisation Name</th>
+                <th>Town/City</th>
+                <th>County</th>
+                <th>Route</th>
+            </tr>
+            <tr>
+                <th>
+                    <input className={styles.input} type='text' placeholder='Search by Organisation Name'/>
+                </th>
+                <th>
+                    <input className={styles.input} type='text' placeholder='Search by Town/City'/>
+                </th>
+                <th>
+                    <input className={styles.input} type='text' placeholder='Search by County'/>
+                </th>
+                <th>
+                    <label htmlFor="routes">Choose a route: </label>
+                    <select id="routes">
+                        <option value="all">All</option>
+                        {
+                            routes.map(route => {
+                                const key = route.split(' ').join('-');
+                                return <option key={key} value={key}>{route}</option>;
+                            })
+                        }
+                    </select>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            {
+                data.content.map(employer => {
+                    return (
+                       <tr key={employer.id}>
+                           <td>{employer.name}</td>
+                           <td>{employer.city}</td>
+                           <td>{employer.county}</td>
+                           <td>{employer.route}</td>
+                       </tr>
+                    )
+                })
+            }
+        </tbody>
+    </table>
 }
